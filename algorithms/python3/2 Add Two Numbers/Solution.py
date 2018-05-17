bfe87079d192530ad66e4f8c4fe4ba8b668165cc
NickWang <hcfw007@126.com>
2018-05-17 17:09:45 +0800

    add Add Two Numbers

---
+++ algorithms/python3/2 Add Two Numbers/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        addOne = 0
+        head = ListNode(0)
+        current = head
+        while ((l1 != None) or (l2 != None) or (addOne != 0)):
+            t = addOne
+            t = t + l1.val if l1 != None else t
+            t = t + l2.val if l2 != None else t
+            if t >= 10:
+                t -= 10
+                addOne = 1
+            else:
+                addOne = 0
+            tempNode = ListNode(t)
+            current.next = tempNode
+            current = tempNode
+            l1 = l1.next if l1 != None else None
+            l2 = l2.next if l2 != None else None
+        return head.next
