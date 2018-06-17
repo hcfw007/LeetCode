f4b165bb11462805d168d592776ed33e34b6a593
NickWang <hcfw007@126.com>
2018-06-17 09:33:56 +0800

    add Length of Last Word

---
+++ algorithms/python3/58 Length of Last Word/Solution.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        s = s.rstrip()
+        ans = s.rfind(" ")
+        if not s: return 0
+        return len(s) if ans < 0 else len(s) - ans - 1
