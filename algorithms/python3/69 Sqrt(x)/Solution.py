862d66715319bea8548a481c8f211ce4d8b0d119
NickWang <hcfw007@126.com>
2018-06-19 10:08:29 +0800

    add Sqrt(x)

---
+++ algorithms/python3/69 Sqrt(x)/Solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def mySqrt(self, x):
+        n = 0
+        while (10 ** n < x): n += 1
+        ans = 0
+        while (n > -1):
+            t = ans + 10 ** n
+            if (t ** 2 > x):
+                n -= 1
+            else:
+                ans = t
+        return ans
