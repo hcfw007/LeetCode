529e4d5d01138855ade6188683bdcd826bd8ebb6
NickWang <hcfw007@126.com>
2018-08-15 15:28:12 +0800

    add 3Sum Closest

---
+++ algorithms/python3/16 3Sum Closest/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def threeSumClosest(self, nums, target):
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        ans = nums[0] + nums[1] + nums[2]
+        for i in range(len(nums) - 2):
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                sum = nums[i] + nums[l] + nums[r]
+                if sum == target:
+                    return sum
+                if abs(sum - target) < abs(ans - target):
+                    ans = sum
+                if sum < target:
+                    j += 1
+                elif sum > target:
+                    k -= 1
+        return ans
