2911fc9a84316cfbf8766baf9a4e43762fa16065
NickWang <hcfw007@126.com>
2018-08-18 16:03:55 +0800

    add 4Sum

---
+++ algorithms/python3/18 4Sum/Solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def fourSum(self, nums, target):
+        nums.sort()
+        results = []
+        self.nSum(nums, target, 4, [], results)
+        return results
+
+    def nSum(self, nums, target, N, result, results):
+        if (len(nums) < N or N < 2): return
+
+        if (N == 2):
+            l, r = 0, len(nums)-1
+            while (l < r):
+                if (nums[l] + nums[r] == target):
+                    results.append(result + [nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while (l < r and nums[l] == nums[l - 1]):
+                        l += 1
+                    while (r > l and nums[r] == nums[r + 1]):
+                        r -= 1
+                elif (nums[l] + nums[r] < target):
+                    l += 1
+                else:
+                    r -= 1
+        else:
+            for i in range(0, len(nums) - N + 1):
+                if (target < nums[i] * N or target > nums[-1] * N):
+                    break
+                if (i == 0 or (i > 0 and nums[i-1] != nums[i])):
+                    self.nSum(nums[i+1:], target-nums[i], N-1, result + [nums[i]], results)
+        return
