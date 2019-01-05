bdf60e231432436bebf692be406d19796e8144c6
hcfw007 <brotherstyx@gmail.com>
2019-01-05 12:17:52 +0800

    add Generate Parentheses

---
+++ algorithms/python3/22 Generate Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        list = []
+
+        def generate(current, pairs, debt):
+            if pairs == 0 and debt == 0:
+                list.append(current)
+                return
+            if pairs > 0:
+                generate(current + '(', pairs - 1, debt + 1)
+            if debt > 0:
+                generate(current + ')', pairs, debt - 1)
+
+        generate('', n, 0)
+        print(list)
