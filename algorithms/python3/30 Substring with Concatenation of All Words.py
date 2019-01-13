f1960f57871e54bd762a2628af9a9d09095609af
hcfw007 <brotherstyx@gmail.com>
2019-01-13 10:35:04 +0800

    add Substring with Concatenation of All Words

---
+++ algorithms/python3/30 Substring with Concatenation of All Words.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        if len(s) == 0 or len(words) == 0: return []
+        word_length = len(words[0])
+        total_length = word_length * len(words)
+        answers = []
+        for i in range(len(s) - total_length + 1):
+            substring_words = []
+            for j in range(len(words)):
+                substring_words.append(s[i + word_length * j: i + word_length * j + word_length])
+            all_words = True
+            _words = words[:]
+            for j in range(len(substring_words)):
+                try:
+                    _words.remove(substring_words[j])
+                except:
+                    break
+            if len(_words) == 0: answers.append(i)
+
+        return answers
