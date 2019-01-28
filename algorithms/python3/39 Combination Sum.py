76c089e4c93d7593b8b24b99fc1b24bbb8e01d5d
hcfw007 <brotherstyx@gmail.com>
2019-01-28 17:42:15 +0800

    add Combination Sum

---
+++ algorithms/python3/39 Combination Sum.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        current = []
+        candidates.sort()
+
+        def generate(numbers: List[int], target: int, threshold: int) -> None:
+            for number in numbers:
+                if number < threshold: continue
+                if number == target:
+                    current.append(number)
+                    ans.append(current[:])
+                    current.pop()
+                    return
+                if number < target:
+                    current.append(number)
+                    generate(numbers, target - number, number)
+                    current.pop()
+                if number > target:
+                    return
+
+        generate(candidates, target, -1)
+        return ans
