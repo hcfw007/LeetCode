8b1614cfe9160c229baf75cdd2b91031650e9f8d
hcfw007 <brotherstyx@gmail.com>
2019-06-01 17:44:17 +0800

    improve Set Matrix Zeroes

--- algorithms/python3/73 Set Matrix Zeroes.py
+++ algorithms/python3/73 Set Matrix Zeroes.py
@@ -1,13 +1,20 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
+        firstCol = False
         for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+            if matrix[i][0] == 0:
+                firstCol = True
+            for j in range(1, len(matrix[0])):
                 if matrix[i][j] == 0:
-                    for x in range(len(matrix)):
-                        if matrix[x][j] != 0: matrix[x][j] = 'zero'
-                    for x in range(len(matrix[0])):
-                        if matrix[i][x] != 0: matrix[i][x] = 'zero'
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+                    
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                    if matrix[i][0] == 0 or matrix[0][j] == 0: matrix[i][j] = 0
 
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == 'zero': matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for j in range(len(matrix[0])): matrix[0][j] = 0
+
+        if firstCol:
+            for i in range(len(matrix)): matrix[i][0] = 0
