7c57167537076d70cf19994cbde5dd964b28eb99
hcfw007 <brotherstyx@gmail.com>
2019-07-03 13:26:02 +0800

    improve Search a 2D Matrix with binary search. However the time consumption increases - -!.

--- algorithms/python3/74 Search a 2D Matrix.py
+++ algorithms/python3/74 Search a 2D Matrix.py
@@ -4,13 +4,32 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         if target < matrix[0][0] or target > matrix[m - 1][n - 1]: return False
-        for i in range(m):
-            if target < matrix[i][0]:
-                for j in range(n):
-                    if matrix[i - 1][j] == target: return True
-                break
-        else:
-            for j in range(n):
-                    if matrix[m - 1][j] == target: return True
+        headList = []
+        for i in range(m): 
+            headList.append(matrix[i][0])
+        rowIndex = binarySearch(headList, target, 0, m - 1, False)
+        return binarySearch(matrix[rowIndex], target, 0, n - 1, True)
 
-        return False
+def binarySearch(arr: List, target: int, head: int, tail: int, equal: bool) -> int:
+    print(head, tail, equal, arr)
+    if tail - head <= 1:
+        if equal:
+            if arr[head] == target: return True
+            if arr[tail] == target: return True
+            return False
+        else:
+            if arr[head] <= target and (head == len(arr) - 1 or arr[head + 1] > target): return head
+            if arr[tail] <= target and (tail == len(arr) - 1 or arr[tail + 1] > target): return tail
+            return False
+
+    t = math.floor((head + tail) / 2)
+    if equal:
+        if arr[t] == target:
+            return True
+    else:
+        if arr[t] <= target and (t == len(arr) - 1 or arr[t + 1] > target):
+            return t
+    if arr[t] < target:
+        return binarySearch(arr, target, t, tail, equal)
+    if arr[t] > target:
+        return binarySearch(arr, target, head, t, equal)
